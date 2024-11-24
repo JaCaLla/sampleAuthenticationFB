b81a8040e5d36cb26c81aaa9aa9c995bd08888d0
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-24 16:43:52 +0100

    Create XCode iOS App blank project

---
+++ sampleAuthenticationFB/sampleAuthenticationFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAuthenticationFBApp.swift
+//  sampleAuthenticationFB
+//
+//  Created by Javier Calatrava on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleAuthenticationFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
