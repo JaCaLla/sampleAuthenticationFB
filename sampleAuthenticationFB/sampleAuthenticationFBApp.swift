daea4d6192fa11cf6fa2d400a1c26dd14c13fd60
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-24 17:28:46 +0100

    Add Firebase SDK to iOS App project

--- sampleAuthenticationFB/sampleAuthenticationFBApp.swift
+++ sampleAuthenticationFB/sampleAuthenticationFBApp.swift
@@ -4,14 +4,29 @@
 //
 //  Created by Javier Calatrava on 24/11/24.
 //
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct sampleAuthenticationFBApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+      WindowGroup {
+        NavigationView {
+          ContentView()
         }
+      }
     }
 }
